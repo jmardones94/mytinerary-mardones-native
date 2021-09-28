d36dc93b02fa72f00d1af982f4fded78a1b4d738
Jonathan Mardones <jmardones.94@gmail.com>
2021-09-27 21:17:14 -0300

    commit commited

---
+++ screens/Cities.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const Cities = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default Cities
+
+const styles = StyleSheet.create({})
---
+++ screens/Itinerary.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const Itinerary = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default Itinerary
+
+const styles = StyleSheet.create({})
